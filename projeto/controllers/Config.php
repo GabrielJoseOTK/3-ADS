97a5165e9e51b80e47a94d196f3396a305df230a
GabrielJoseOTK <95941462+GabrielJoseOTK@users.noreply.github.com>
2022-04-24 07:22:28 -0300

    Add files via upload

---
+++ projeto/controllers/Config.php
@@ -0,0 +1,23 @@
+<?php
+    if(!defined('HOST')){
+        define('HOST','localhost');   
+    }
+    if(!defined('USER')){
+        define('USER','root2');  
+    }
+    if(!defined('PASS')){
+        define('PASS','');  
+    }
+    if(!defined('BD')){
+        define('BD','Siteedu'); 
+    }
+    
+
+
+
+
+
+
+
+
+?>
